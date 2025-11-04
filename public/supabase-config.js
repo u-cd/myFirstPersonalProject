983c3cc5341e61f3e5e02c47dca2d35272d4b6ac
ue-jpg <pitesdfghjk@gmail.com>
2025-11-05 01:32:42 +0900

    start to use Supabase for authentication, only login page created

---
+++ public/supabase-config.js
@@ -0,0 +1,4 @@
+// Supabase configuration file
+// Use this for frontend Supabase client initialization
+export const SUPABASE_URL = 'https://uesmrlothnuxcxfsjget.supabase.co';
+export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVlc21ybG90aG51eGN4ZnNqZ2V0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIxNzQ3NjEsImV4cCI6MjA3Nzc1MDc2MX0.nWMZtSd9iwyN2QGNgYnuHpPnUfQ3vyg8tsHAjz78q0g';
